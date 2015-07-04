606bf87521cb9661b5fbf18887ffda2c9b700b76
jeovajr <jeova.goncalves@homail.com>
2015-07-04 11:08:29 -0300

    Adicionada Função em main.c

--- main.c
+++ main.c
@@ -2,8 +2,14 @@
 
 #include <stdio.h>
 
+void helloJeova();
+
 void main(){
     printf("Hello Git no sabadão!");
 }
 
 //Comentario feito po Frederico
+
+void helloJeova(){
+	printf("Hello Git Jeová");
+}
